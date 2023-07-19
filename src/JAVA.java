bbbe5a0ea1cf37903d7ffaaf9c2615d1a923cf67
omsaraswat2601 <139962023+omsaraswat2601@users.noreply.github.com>
2023-07-19 14:38:50 +0530

    done

---
+++ src/JAVA.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class JAVA {
+	
+	public static void main(String[]args) {
+	List<Integer> list=new LinkedList();
+	list.add(10);
+	list.add(12);
+	list.add(13);
+	
+	for (Integer o:list)
+	{
+		System.out.println(0+1);
+		
+	}
+	}
+}
